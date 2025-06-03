fb168a01d74cb1f97d6db6440f57c37029fd49e8
Pornnapa-Panyadee <pornnapa.van@gmail.com>
2025-06-03 23:38:22 +0700

    db

--- Config/db.js
+++ Config/db.js
@@ -1,12 +1,13 @@
 // Config/db.js
 const mysql = require('mysql2');
+require('dotenv').config(); // Load environment variables from .env file
 
 // Create a connection to the MySQL database
 const db = mysql.createConnection({
-    host: 'localhost', // Replace with your MySQL host
-    user: 'root',      // Replace with your MySQL user
-    password: '',      // Replace with your MySQL password
-    database: 'cfp_data' // Replace with your database name
+    host: process.env.DB_HOST, // Replace with your MySQL host
+    user: process.env.DB_USER,      // Replace with your MySQL user
+    password: process.env.DB_PASSWORD,      // Replace with your MySQL password
+    database: process.env.DB_DATABASE // Replace with your database name
 });
 
 // Open the connection to the MySQL database
